f9d9c78b5e7673d2addc2bfe6bf0bce43bda1c6b
cepongeseng <kangcepon45@gmail.com>
2024-11-24 14:11:47 +0700

    make the bach button from tree to home,and make it only accept png file

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from io import BytesIO
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads'
-app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['ALLOWED_EXTENSIONS'] = {'png'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
